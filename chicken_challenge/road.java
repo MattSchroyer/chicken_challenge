c7c820dafa1b41c8ba4b76048a82684f5b4a11be
Matt Schroyer <mschroyer@gmail.com>
2018-11-24 15:32:32 -0600

    Subclasses created
    Added most all of the classes/subclasses needed to build the rest of the game. Some might be necessary later, but this covers most of them.

--- chicken_challenge/road.java
+++ chicken_challenge/road.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class road here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends greenfoot.World
+public class road extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class road.
      * 
      */
-    public MyWorld()
+    public road()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
