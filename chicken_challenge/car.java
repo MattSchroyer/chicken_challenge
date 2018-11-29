562ffcf8c8b31c95981da6664fe922c870ec47d5
Matt Schroyer <mschroyer@gmail.com>
2018-11-28 22:20:05 -0600

    Re structured car and chicken classes
    Car and chicken classes used to be subclasses of "movingActor", but now I'm thinking they can just be classes under Actor instead.

--- chicken_challenge/car.java
+++ chicken_challenge/car.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class vehicle here.
+ * Write a description of class car here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class vehicle extends movingActor
+public class car extends Actor
 {
     /**
-     * Act - do whatever the vehicle wants to do. This method is called whenever
+     * Act - do whatever the car wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- chicken_challenge/chicken.java
+++ chicken_challenge/chicken.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class chicken extends movingActor
+public class chicken extends Actor
 {
     /**
      * Act - do whatever the chicken wants to do. This method is called whenever
