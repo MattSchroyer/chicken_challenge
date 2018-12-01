c17bbeb57670c046a0fa487d580d3dd84c324125
Matt Schroyer <mschroyer@gmail.com>
2018-12-01 12:30:01 -0600

    Added points and splatter to chicken
    Chicken now receives points for moving forward across the road. It also will change its image to the "splatter" when it has been hit by a car.

--- chicken_challenge/chicken.java
+++ chicken_challenge/chicken.java
@@ -12,16 +12,22 @@ public class chicken extends Actor
      * Act - do whatever the chicken wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+
+   GreenfootImage splat = new GreenfootImage("Splat.png");
+
     public void act()
     {
         // Add your action code here.
         keyCheck();
+        collisionCheck();
     }
 
     public void keyCheck()
     {
       if (Greenfoot.isKeyDown("up")) {
+        road myRoad = (road)getWorld();
         setLocation(getX(), getY() - 4);
+        myRoad.increaseScore(10);
       }
       if (Greenfoot.isKeyDown("down"))
       {
@@ -34,6 +40,18 @@ public class chicken extends Actor
       if (Greenfoot.isKeyDown("left"))
       {
         setLocation(getX()-4, getY());
+      }
+    }
+
+    public void collisionCheck()
+    {
+      if (isTouching(carL.class)) {
+        setImage(splat);
+        // myRoad.splatteredChicken
+      }
+      if (isTouching(carR.class)) {
+        setImage(splat);
+        // myRoad.splatteredChicken
       }
     }
 }
